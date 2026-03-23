f875475ded683b3399eb4a33cd7f61dcaa0e62e4
iamDrakkir <Hagelin.Rickard@gmail.com>
2026-03-23 10:20:41 +0100

    proton-pass-cli setup

--- home/common/optional/tools/proton.nix
+++ home/common/optional/tools/proton.nix
@@ -8,6 +8,19 @@
   home.packages = with pkgs; [
     proton-pass
     protonvpn-gui
-    proton-pass-cli
   ];
+
+  services.proton-pass-agent = {
+    enable = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  # The agent fails on boot if not logged in yet; keep retrying until it succeeds
+  systemd.user.services.proton-pass-agent = {
+    Service = {
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
 }
