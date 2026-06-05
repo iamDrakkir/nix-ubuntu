f545d30478054e100d88b4450b05531978992237
Rickard Hagelin <rickard.hagelin@ctek.com>
2026-06-05 20:48:14 +0200

    rose-pine and teams fix

--- home/common/optional/programs/teams.nix
+++ home/common/optional/programs/teams.nix
@@ -2,6 +2,16 @@
 
 {
   home.packages = with pkgs; [
-        teams-for-linux
+    teams-for-linux
   ];
+
+  xdg.desktopEntries.teams-for-linux = {
+    name = "Microsoft Teams for Linux";
+    comment = "Unofficial Microsoft Teams client for Linux";
+    exec = "teams-for-linux --class=teams-for-linux %U";
+    icon = "teams-for-linux";
+    type = "Application";
+    categories = [ "Network" "InstantMessaging" "Chat" ];
+    mimeType = [ "x-scheme-handler/msteams" ];
+  };
 }
